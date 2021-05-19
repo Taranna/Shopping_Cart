25a625db882dbd7d1a2ba5b1089dfebad876d161
Tara Mathews <52750864+Taranna@users.noreply.github.com>
2021-05-19 17:18:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,19 @@ connectDB();
 const app=express();
 app.use(express.json());
 app.use("/data/products",productRoutes);
-
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,'../client/build')));
+  
+    app.get('*',(req,res) => {
+      res.sendFile(path.join(__dirname,"..",'client','build','index.html'))
+    })
+  }
+  else{
+    app.get('/',(req,res)=>{
+      res.send("App is running")
+  
+    })
+  }
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
